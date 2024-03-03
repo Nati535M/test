eb02a574923b49059ee73ac03864b07d7ad3feda
Nati535M <nati5352604@gmail.com>
2024-03-03 18:06:57 +0200

    start  selenium

---
+++ src/test/java/LoginTests.java
@@ -0,0 +1,50 @@
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class LoginTests {
+    @BeforeClass
+    public void preCondition() {
+        //open bbouser
+        //open site
+
+
+    }
+    @Test
+    public void loginSuccess(){
+        //open form(find element+click)
+        //fill login(find element+click+clear+type)
+        //fill password(find element+click+clear+type)
+        //submitform(find element+click)
+    }
+    @Test
+    public void loginWrongEmail(){
+        //open form(find element+click)
+        //fill login(find element+click+clear+type)
+        //fill password(find element+click+clear+type)
+        //submitform(find element+click)
+    }
+    @Test
+    public void loginWrongPassword(){
+        //open form(find element+click)
+        //fill login(find element+click+clear+type)
+        //fill password(find element+click+clear+type)
+        //submitform(find element+click)
+
+        //submitform
+
+
+    }
+    @AfterClass
+    public void postCondition(){
+        //close brouser
+
+
+
+    }
+
+
+
+
+
+}
